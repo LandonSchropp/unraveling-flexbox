159da67bfc8f3750af2ccf1984e4966f970ec8c9
Landon Schropp <schroppl@gmail.com>
2015-07-19 14:23:46 -0700

    Add Autoprefixer

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem "middleman", "~> 3.3.12"
 gem "middleman-livereload", "~> 3.1.0"
+gem 'middleman-autoprefixer'
--- config.rb
+++ config.rb
@@ -1,5 +1,10 @@
 activate :automatic_image_sizes
 
+activate :autoprefixer do |config|
+  config.browsers = ['last 2 versions', 'Explorer >= 10', 'Android >= 4.1', 'Safari >= 7',
+    'iOS >= 7']
+end
+
 configure :development do
   activate :livereload
 end
