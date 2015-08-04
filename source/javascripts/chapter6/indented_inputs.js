e3bf65014c17e92908c244bdad7bc15b947720cb
Landon Schropp <schroppl@gmail.com>
2015-08-03 20:38:17 -0700

    Remove the indented inputs example

--- source/javascripts/chapter6/indented_inputs.js
+++
@@ -1,4 +0,0 @@
-$('input[type="checkbox"]:not([data-parent])').click(function() {
-  var $indentedChildren = $('[data-parent="' + $(this).attr('id') + '"]');
-  $indentedChildren.attr("disabled", !$(this).is(':checked'));
-});
